bb1f9f6e8338c45a7b114ee4d26934f45cfcb2f7
chris24xd <christosandreou96@yahoo.com>
2026-05-03 15:51:58 +0300

    Refactored PreReferencer

--- Assets/Scripts/References/PreReferencer.cs
+++ Assets/Scripts/References/PreReferencer.cs
@@ -4,12 +4,11 @@ namespace References
 {
     public class PreReferencer : MonoBehaviour
     {
-        public static PreReferencer Instance => _instance;
-        static PreReferencer _instance;
+        public static PreReferencer Instance { get; private set; }
 
         public AudioClip[] miscClips; // 0 = Jumpscare, 1 = Shock, 2 = Jumpscare Boom, 3 = Mark
         public Sprite defaultMark;
     
-        private void Awake() => _instance = this;
+        private void Awake() => Instance = this;
     }
 }
