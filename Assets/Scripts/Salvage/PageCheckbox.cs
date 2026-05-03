cba8753553f1397d3183548a51f836e9926fe9a1
chris24xd <christosandreou96@yahoo.com>
2026-05-03 15:06:08 +0300

    Refactored PageCheckbox
    Renamed badly named variables.
    Did some code spacing.

--- Assets/Scripts/Salvage/PageCheckbox.cs
+++ Assets/Scripts/Salvage/PageCheckbox.cs
@@ -7,26 +7,28 @@ namespace Salvage
 {
     public class PageCheckbox : MonoBehaviour, IPointerClickHandler
     {
-        private Image x;
-        private Animator anim;
+        private Image _checkImage;
+        private Animator _animator;
 
         private void Awake()
         {
-            x = GetComponent<Image>();
-            anim = GetComponent<Animator>();
+            _checkImage = GetComponent<Image>();
+            _animator = GetComponent<Animator>();
         }
+        
         public void OnPointerClick(PointerEventData eventData)
         {
-            x.raycastTarget = false;
-            anim.Play("X");
+            _checkImage.raycastTarget = false;
+            _animator.Play("X");
             AudioSource.PlayClipAtPoint(PreReferencer.Instance.miscClips[3], Camera.main.transform.position);
         }
 
         public void OnDisable()
         {
-            if (x.raycastTarget) return;
-            x.sprite = PreReferencer.Instance.defaultMark;
-            Destroy(anim);
+            if (_checkImage.raycastTarget) return;
+            
+            _checkImage.sprite = PreReferencer.Instance.defaultMark;
+            Destroy(_animator);
         }
     }
 }
